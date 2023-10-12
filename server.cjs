f8f6658fee31c8835a1161a515c1118bea810792
micsta00 <110664760+micsta00@users.noreply.github.com>
2023-10-12 13:49:11 +0200

    can now send message to all, room, or direct to some user

--- server.cjs
+++ server.cjs
@@ -7,12 +7,17 @@ const io = require('socket.io')(3000, {
 io.on('connection', socket => {
     console.log(socket.id)
     socket.emit('chat-message', 'Hello World')
-    socket.on('send-message', (message) => {
-        // to everyone including sender
-        // io.emit('receive-message', message)
+    socket.on('send-message', (message, room) => {
+        if (room === '') {
+            // to everyone including sender
+            // io.emit('receive-message', message)
 
-        // to everyone except sender
-        socket.broadcast.emit('receive-message', message)
+            // to everyone except sender
+            socket.broadcast.emit('receive-message', message)
+        } else {
+            // to users in the room except sender | or to specific user if we write receiver-id in room field
+            socket.to(room).emit('receive-message', message)
+        }
 
         console.log(message)
     })
