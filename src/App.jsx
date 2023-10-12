76d52fe01a5a60b42398bc3b6a5561eb3f949723
micsta00 <110664760+micsta00@users.noreply.github.com>
2023-10-12 11:57:27 +0200

    added basic styles, added nodemon and server.js

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ function App() {
 
 
   return (
-    <>
-      <div>
-
-      </div>
-    </>
+    <div id="main-container">
+      <div id="message-container"></div>
+      <form id="send-container">
+        <input type="text" id="message-input" />
+        <button type="submit" id="send-button">Send</button>
+      </form>
+    </div>
   )
 }
 
